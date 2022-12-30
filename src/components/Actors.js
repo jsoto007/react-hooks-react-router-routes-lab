bee3968a691c146087ec3ea9e8a08eea0e3221fd
Jose Soto <jsoto7087@gmail.com>
2022-12-29 20:20:14 -0500

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
+  console.log(actors)
+  const newActorItem = actors.map((actor) => {
+  return ( <div key={actor.name}>
+      {actor.name}
+     <ul>{actor.movies}</ul> 
+    </div>)
+  })
+  console.log(newActorItem)
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Actos Page</h1>
+      {newActorItem}
     </div>
   )
 }
